6d33f9977ea3efb70e860372b156073d9d7a3748
Benjamin Kaganovitch <BKaganovit@smail.pembinatrails.ca>
2023-04-23 12:57:08 -0500

    I added a Click event that for all operations. I made the clear buttons make the field zero.
    The label on top partially works.
    
    Have not done anything for the radio buttons yet, besides research.
    
    Im in the midst of optimizing the code for each of the number buttons, by adding the same click event to each number button like for the operation buttons. This will shorten the code.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -20,6 +20,15 @@
             base.Dispose(disposing);
         }
 
+        //a: what do I do to make the button interact with the textbox? 
+        //a: You need to add a click event to the button and then add the value of the button to the textbox
+        //a: dont I already have that?
+        //a: No, you have a click event for the button but you need to add the value of the button to the textbox
+        //a: can we do that here?
+        //a: No, you need to do that in the designer
+        //a: where do I navigate to? 
+        //a: You need to double click on the button and it will take you to the designer
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -37,14 +46,22 @@
             button7 = new Button();
             button8 = new Button();
             button9 = new Button();
-            button10 = new Button();
             button11 = new Button();
             button12 = new Button();
             button13 = new Button();
             button14 = new Button();
             button17 = new Button();
             button18 = new Button();
-            textBox1 = new TextBox();
+            groupBox1 = new GroupBox();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            labelCurrentOperation = new Label();
+            Answere = new TextBox();
+            button15 = new Button();
+            button16 = new Button();
+            button10 = new Button();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
@@ -81,6 +98,7 @@
             button3.TabIndex = 2;
             button3.Text = "3";
             button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click_1;
             // 
             // button4
             // 
@@ -92,6 +110,7 @@
             button4.TabIndex = 3;
             button4.Text = "4";
             button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
             // 
             // button5
             // 
@@ -103,6 +122,7 @@
             button5.TabIndex = 4;
             button5.Text = "5";
             button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click_1;
             // 
             // button6
             // 
@@ -114,6 +134,7 @@
             button6.TabIndex = 5;
             button6.Text = "6";
             button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click_1;
             // 
             // button7
             // 
@@ -125,6 +146,7 @@
             button7.TabIndex = 6;
             button7.Text = "7";
             button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click_1;
             // 
             // button8
             // 
@@ -136,6 +158,7 @@
             button8.TabIndex = 7;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
             // 
             // button9
             // 
@@ -149,18 +172,6 @@
             button9.UseVisualStyleBackColor = false;
             button9.Click += button9_Click;
             // 
-            // button10
-            // 
-            button10.BackColor = Color.SeaGreen;
-            button10.Font = new Font("TI-Nspire Sans", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            button10.ForeColor = Color.SeaGreen;
-            button10.Location = new Point(457, 94);
-            button10.Name = "button10";
-            button10.Size = new Size(92, 70);
-            button10.TabIndex = 9;
-            button10.Text = "=";
-            button10.UseVisualStyleBackColor = false;
-            // 
             // button11
             // 
             button11.BackColor = Color.SeaGreen;
@@ -171,6 +182,7 @@
             button11.TabIndex = 10;
             button11.Text = "+";
             button11.UseVisualStyleBackColor = false;
+            button11.Click += PerformOperation;
             // 
             // button12
             // 
@@ -182,7 +194,7 @@
             button12.TabIndex = 11;
             button12.Text = "-";
             button12.UseVisualStyleBackColor = false;
-            button12.Click += button12_Click;
+            button12.Click += PerformOperation;
             // 
             // button13
             // 
@@ -194,7 +206,7 @@
             button13.TabIndex = 12;
             button13.Text = "/";
             button13.UseVisualStyleBackColor = false;
-            button13.Click += button13_Click;
+            button13.Click += PerformOperation;
             // 
             // button14
             // 
@@ -206,6 +218,7 @@
             button14.TabIndex = 13;
             button14.Text = "*";
             button14.UseVisualStyleBackColor = false;
+            button14.Click += PerformOperation;
             // 
             // button17
             // 
@@ -226,15 +239,113 @@
             button18.Name = "button18";
             button18.Size = new Size(92, 70);
             button18.TabIndex = 17;
-            button18.Text = "Clr";
+            button18.Text = "C";
             button18.UseVisualStyleBackColor = false;
+            button18.Click += button18_Click;
             // 
-            // textBox1
+            // groupBox1
             // 
-            textBox1.Location = new Point(163, 61);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(300, 27);
-            textBox1.TabIndex = 19;
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(576, 94);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 125);
+            groupBox1.TabIndex = 20;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoCheck = false;
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(12, 87);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(56, 24);
+            radioButton3.TabIndex = 2;
+            radioButton3.Text = "LOC";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoCheck = false;
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(12, 57);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(54, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.Text = "BIN";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoCheck = false;
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Location = new Point(12, 30);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(58, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "DEC";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // labelCurrentOperation
+            // 
+            labelCurrentOperation.AutoSize = true;
+            labelCurrentOperation.Location = new Point(159, 23);
+            labelCurrentOperation.Name = "labelCurrentOperation";
+            labelCurrentOperation.Size = new Size(157, 20);
+            labelCurrentOperation.TabIndex = 22;
+            labelCurrentOperation.Text = "labelCurrentOperation";
+            labelCurrentOperation.Click += label1_Click_1;
+            // 
+            // Answere
+            // 
+            Answere.Location = new Point(167, 60);
+            Answere.Name = "Answere";
+            Answere.Size = new Size(125, 27);
+            Answere.TabIndex = 23;
+            Answere.TextChanged += Answere_TextChanged;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.SeaGreen;
+            button15.Font = new Font("TI-Nspire Sans", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            button15.Location = new Point(65, 246);
+            button15.Name = "button15";
+            button15.Size = new Size(92, 70);
+            button15.TabIndex = 24;
+            button15.Text = "0";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click_1;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.SeaGreen;
+            button16.Font = new Font("TI-Nspire Sans", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            button16.Location = new Point(554, 322);
+            button16.Name = "button16";
+            button16.Size = new Size(92, 70);
+            button16.TabIndex = 25;
+            button16.Text = "CE";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button16_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.SeaGreen;
+            button10.Font = new Font("TI-Nspire Sans", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(457, 97);
+            button10.Name = "button10";
+            button10.Size = new Size(92, 70);
+            button10.TabIndex = 26;
+            button10.Text = "=";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
             // 
             // Form1
             // 
@@ -242,14 +353,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DimGray;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
+            Controls.Add(button10);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(Answere);
+            Controls.Add(labelCurrentOperation);
+            Controls.Add(groupBox1);
             Controls.Add(button18);
             Controls.Add(button17);
             Controls.Add(button14);
             Controls.Add(button13);
             Controls.Add(button12);
             Controls.Add(button11);
-            Controls.Add(button10);
             Controls.Add(button9);
             Controls.Add(button8);
             Controls.Add(button7);
@@ -261,6 +376,8 @@
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -276,13 +393,20 @@
         private Button button7;
         private Button button8;
         private Button button9;
-        private Button button10;
         private Button button11;
         private Button button12;
         private Button button13;
         private Button button14;
         private Button button17;
         private Button button18;
-        private TextBox textBox1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label labelCurrentOperation;
+        private TextBox Answere;
+        private Button button15;
+        private Button button16;
+        private Button button10;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,10 @@ namespace CalculatorVerstion2
 {
     public partial class Form1 : Form
     {
+
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
         public Form1()
         {
             InitializeComponent();
@@ -9,44 +13,234 @@ namespace CalculatorVerstion2
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "1";
+            isOperationPerformed = false;
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "2";
+            isOperationPerformed = false;
+            
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "3";
+            isOperationPerformed = false;
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "4";
+            isOperationPerformed = false;
+            
+        }
+
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "5";
+            isOperationPerformed = false;
+            
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "6";
+            isOperationPerformed = false;
+            
+        }
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "7";
+            isOperationPerformed = false;
+            
+        }
+
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "8";
+            isOperationPerformed = false;
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "9";
+            isOperationPerformed = false;
+            
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "9";
+            isOperationPerformed = false;
+            
+        }
+
+        // This button is for zero
+        private void button15_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Answere.Text = Answere.Text + "0";
+            isOperationPerformed = false;
+            
+        }
+
+
+
+
+        
+
+
+
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // DEC radio button
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            // HEX radio button
+            radioButton1.Checked = false;
+            radioButton3.Checked = false;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            // OCT radio button
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
 
         }
 
+
+
+
+
+        
+
+
+
+        /*
+         * 
+         * The following code is for the operation buttons: +, -, *, /
+         */
+        private void PerformOperation(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operationPerformed = button.Text;
+            resultValue = Double.Parse(Answere.Text);
+            labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+            isOperationPerformed = true;
+        }
+
+        // CE button
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Answere.Text = "0";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Answere.Text = "0";
+            resultValue = 0;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    Answere.Text = (resultValue + Double.Parse(Answere.Text)).ToString();
+                    break;
+                case "-":
+                    Answere.Text = (resultValue - Double.Parse(Answere.Text)).ToString();
+                    break;
+                case "*":
+                    Answere.Text = (resultValue * Double.Parse(Answere.Text)).ToString();
+                    break;
+                case "/":
+                    Answere.Text = (resultValue / Double.Parse(Answere.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+
+
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void button12_Click_1(object sender, EventArgs e)
+        {
+
+
+        }
+        private void button13_Click_1(object sender, EventArgs e)
+        {
+
+
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+
+        }
         private void button12_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void button13_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button15_Click(object sender, EventArgs e)
         {
 
         }
 
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        //Answere text box
+        private void Answere_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button10_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
+
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private void button13_Click(object sender, EventArgs e)
         {
 
         }
